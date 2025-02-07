2f00743db3a67d7ff3527bafcbb307fbf46e694a
Grom1806 <s76945964@gmail.com>
2025-02-08 00:14:29 +0100

    remove vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { StrictMode } from 'react';
-import App from './App.jsx';
-import { store } from './redux/store.js';
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <SpeedInsights>
-          <App />
-        </SpeedInsights>
-      </Provider>
-    </BrowserRouter>
-  </StrictMode>
-);
+	<StrictMode>
+		<BrowserRouter>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</BrowserRouter>
+	</StrictMode>
+)
