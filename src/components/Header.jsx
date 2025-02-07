7f444b75354431cfdd9b992c6bbb73ab001dab73
Grom1806 <s76945964@gmail.com>
2025-02-08 00:07:51 +0100

    update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
 				<Link to='/' className='header__logo'>
 					<img width='38' src={logo} alt='Pizza logo' />
 					<div>
-						<h1>React Pizza V2</h1>
+						<h1>React Pizza </h1>
 						<p>самая вкусная пицца во вселенной</p>
 					</div>
 				</Link>
